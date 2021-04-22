440689320f47de885b17218140e4262b8ee9e5f5
Victor Morales <chipahuac@hotmail.com>
2021-04-22 11:37:10 -0700

    Fix tinkerbell deployment issues

--- tinkerbell/deploy.sh
+++ tinkerbell/deploy.sh
@@ -13,7 +13,12 @@ set -o xtrace
 set -o errexit
 set -o nounset
 
+# shellcheck disable=SC1091
+source /etc/environment
+
 export TINKERBELL_CERTS_PATH=/var/local/certs
+max_attempts=3
+mac_address="00:00:00:00:00:02"
 
 sudo docker-compose up -d
 
@@ -21,16 +26,41 @@ sudo docker-compose up -d
 sudo docker login --username "${REGISTRY_USERNAME:-docker}" --password "${REGISTRY_PASSWORD:-secret}" http://localhost:5000
 while IFS= read -r image; do
     image_name="${image#*/}"
-    if [ "$(curl "http://localhost:5000/v2/${image_name%:*}/tags/list" -o /dev/null -w '%{http_code}\n' -s)" != "200" ]; then
+    if [ "$(curl --user "${REGISTRY_USERNAME:-docker}:${REGISTRY_PASSWORD:-secret}" "http://localhost:5000/v2/${image_name%:*}/tags/list" -o /dev/null -w '%{http_code}\n' -s)" != "200" ]; then
         skopeo copy --dest-tls-verify=false "docker://$image" "docker://localhost:5000/$image_name"
     fi
 done < actions.txt
 
-if tink template get --no-headers | grep -q hello_world_workflow; then
-    tink template create --file hello-world.tmpl
-fi
+# Create tinkerbell template
+attempt_counter=0
+templates=$(tink template get --no-headers)
+until echo "$templates" | grep -q hello_world_workflow; do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+        echo "Max attempts reached"
+        exit 1
+    fi
+    < hello-world.tmpl tink template create || true
+
+    attempt_counter=$((attempt_counter+1))
+    sleep 5
+    templates=$(tink template get --no-headers)
+done
 template_id=$(tink template get --no-headers | grep hello_world_workflow | awk -F '|' '{ print $2}' | xargs)
-tink hardware push --file testvm.json
-if tink workflow get --no-headers | grep -q 00:00:00:00:00:02; then
-    tink workflow create --template "$template_id" --hardware '{"device_1": "00:00:00:00:00:02"}'
-fi
+
+# Register Hardware
+attempt_counter=0
+hardware=$(tink hardware get --no-headers)
+until echo "$hardware" | grep -q "$mac_address" ; do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+        echo "Max attempts reached"
+        exit 1
+    fi
+    < testvm.json tink hardware push || true
+    attempt_counter=$((attempt_counter+1))
+    sleep 5
+    hardware=$(tink hardware get --no-headers)
+done
+
+# Create tinkerbell workflow
+workflows=$(tink workflow get --no-headers)
+echo "$workflows" | grep -q "$template_id" || tink workflow create --template "$template_id" --hardware "{\"device_1\": \"$mac_address\"}"
