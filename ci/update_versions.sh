6d3ad35d3a79e76511a5e05c977945865eefdd27
Victor Morales <chipahuac@hotmail.com>
2022-06-03 13:26:52 -0700

    Enable update CI workflow

---
+++ ci/update_versions.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2022
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o errexit
+set -o pipefail
+if [[ "${DEBUG:-false}" == "true" ]]; then
+    set -o xtrace
+fi
+
+function get_version {
+    local type="$1"
+    local name="$2"
+    local version=""
+    local attempt_counter=0
+    readonly max_attempts=5
+
+    until [ "$version" ]; do
+        version=$("_get_latest_$type" "$name")
+        if [ "$version" ]; then
+            break
+        elif [ ${attempt_counter} -eq ${max_attempts} ];then
+            echo "Max attempts reached"
+            exit 1
+        fi
+        attempt_counter=$((attempt_counter+1))
+        sleep $((attempt_counter*2))
+    done
+
+    echo "${version#v}"
+}
+
+function _get_latest_github_release {
+    url_effective=$(curl -sL -o /dev/null -w '%{url_effective}' "https://github.com/$1/releases/latest")
+    if [ "$url_effective" ]; then
+        echo "${url_effective##*/}"
+    fi
+}
+
+function _get_latest_github_tag {
+    tags="$(curl -s "https://api.github.com/repos/$1/tags")"
+    if [ "$tags" ]; then
+        echo "$tags" | grep -Po '"name":.*?[^\\]",' | awk -F  "\"" 'NR==1{print $4}'
+    fi
+}
+
+sed -i "s/export TINKERBELL_VERSION=.*/export TINKERBELL_VERSION=$(get_version github_tag tinkerbell/tink)/g" ./tinkerbell/defaults.env
+sed -i "s/export CFSSL_VERSION=.*/export CFSSL_VERSION=$(get_version github_release cloudflare/cfssl)/g" ./tinkerbell/defaults.env
